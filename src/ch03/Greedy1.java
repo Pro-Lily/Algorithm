13c6525e180ba0c24563b6253169bacbb1e42097
Judy <programmer_ls@naver.com>
2022-01-05 01:03:28 +0900

    예제 3-1 거스름돈

---
+++ src/ch03/Greedy1.java
@@ -0,0 +1,20 @@
+package ch03;
+
+import java.util.Scanner;
+
+// °Å½º¸§µ·
+public class Greedy1 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int money[] = {500, 100, 50, 10};
+		int cnt = 0;
+		
+		for(int i=0; i<money.length; i++) {
+			int coin = money[i]; // 500 100 50 10
+			cnt += n / coin; // 2 4 5 6
+			n = n % coin; // 260 60 10
+		}
+	}
+}
