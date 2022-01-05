dfca80c456b92180a5666747d749e87aa4c316be
Judy <programmer_ls@naver.com>
2022-01-05 22:35:53 +0900

    3-3) 숫자 카드 게임

--- src/ch03/Greedy3.java
+++ src/ch03/Greedy3.java
@@ -3,7 +3,7 @@ package ch03;
 import java.util.Arrays;
 import java.util.Scanner;
 
-// ¼ıÀÚ Ä«µå °ÔÀÓ
+// ìˆ«ì ì¹´ë“œ ê²Œì„
 public class Greedy3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
