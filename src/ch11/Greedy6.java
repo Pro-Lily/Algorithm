e5b48fda343f7ad968555d617bef0ca51bfaf0b0
Judy <programmer_ls@naver.com>
2022-01-20 02:36:18 +0900

    11-6) 무지의 먹방 라이브 (효율성 안좋음)

--- src/ch11/Greedy6.java
+++ src/ch11/Greedy6.java
@@ -5,16 +5,15 @@ package ch11;
 public class Greedy6 {
 	static int solution(int[] food_times, long k) {
 		// 더 섭취해야 할 음식이 없다면 -1을 반환
-        long summary = 0;
+        long zero = 0;
         for (int i = 0; i < food_times.length; i++) {
-            summary += food_times[i];
+            zero += food_times[i];
         }
-        if (summary <= k) return -1;
-		
-		k += 1;
+        if (zero <= k) return -1;
+        
 		int answer = 0;
-		int time = 0;
-		int status = 0;
+		int time = -1;
+		
 		while (true) {
 			for (int i = 0; i < food_times.length; i++) {
 				if(k==time) {
@@ -24,10 +23,8 @@ public class Greedy6 {
 					continue;
 				}
 				food_times[i] -= 1;
-				System.out.println("배열 안에: " + food_times[i]);
 				time++;
-				System.out.println("현재 시간은: " + time);
-				answer = i+1;
+				answer = i+1; // 0번째니까 +1
 			}
 			if(k==time) {
 				break;
@@ -37,8 +34,10 @@ public class Greedy6 {
 	}
 
 	public static void main(String[] args) {
-		int time[] = { 3, 1, 2 }; // 각 음식을 모두 먹는데 필요한 시간이 음식의 번호 순서대로 들어 있는 배열
-		long k = 5; // 방송이 중단된 시간
+		// 각 음식을 모두 먹는데 필요한 시간이 음식의 번호 순서대로 들어 있는 배열
+		int time[] = { 3, 1, 2 }; 
+		// 방송이 중단된 시간
+		long k = 5; 
 
 		int result = solution(time, k);
 		System.out.println(result);
